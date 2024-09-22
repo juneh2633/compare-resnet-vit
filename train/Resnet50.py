522dd5c17551f831cb4d39699f3eb5e9036004e9
juneh2633 <juneh2633@gmail.com>
2024-09-22 17:43:54 +0900

    fix:resnet leaning rate:

--- train/Resnet50.py
+++ train/Resnet50.py
@@ -16,7 +16,7 @@ def resnet_train():
     num_classes = 10
     batch_size = 32
     num_epochs = 25
-    learning_rate = 0.001
+    learning_rate = 1e-5
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
