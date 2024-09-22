aac7b5e5655402b48b01a0f861770fb800a1f513
juneh2633 <juneh2633@gmail.com>
2024-09-22 16:29:20 +0900

    feat: cmd

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from compare import compare_models
 from train.Resnet50 import resnet_train
 from train.ViT import vit_train
 
-vit_train()
-resnet_train()
+
+def main():
+
+    while 1:
+        print("1. train ")
+        print("2. compare ")
+        a = int(input("input 1 or 2 \n"))
+        if a == 1:
+            vit_train()
+            resnet_train()
+            break
+        elif a == 2:
+            compare_models()
+            break
+
+
+if __name__ == "__main__":
+    main()
