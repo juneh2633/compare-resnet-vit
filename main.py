ffa7b5f271653c140be80959512a6181e696c6e3
juneh2633 <juneh2633@gmail.com>
2024-09-23 17:35:30 +0900

    chore: model pretrained to weightspython3

--- main.py
+++ main.py
@@ -5,27 +5,8 @@ from train.vit_not_finetuning import vit_test
 
 
 def main():
-    # vit_test()
-    while 1:
-        print("1. train ")
-        print("2. compare ")
-        a = int(input("input 1 or 2 \n"))
-        if a == 1:
-            print("1. vit ")
-            print("2. resnet50 ")
-            print("3. all ")
-            x = int(input("input 1 or 2 \n"))
-            if x == 1:
-                vit_train()
-            elif x == 2:
-                resnet_train()
-            elif x == 3:
-                vit_train()
-                resnet_train()
-            break
-        elif a == 2:
-            compare_models()
-            break
+    vit_train()
+    resnet_train()
 
 
 if __name__ == "__main__":
